86be9b9e33fd77680978106072251e0f2b391c78
Florian Goujeon <florian.goujeon@gmail.com>
2018-10-28 17:05:33 +0100

    Add Boost Software License

--- fgl_signal/include/fgl/signal.hpp
+++ fgl_signal/include/fgl/signal.hpp
@@ -1,3 +1,9 @@
+//Copyright Florian Goujeon 2018.
+//Distributed under the Boost Software License, Version 1.0.
+//(See accompanying file LICENSE_1_0.txt or copy at
+//https://www.boost.org/LICENSE_1_0.txt)
+//Official repository: https://github.com/fgoujeon/signal
+
 #ifndef FGL_SIGNAL_HPP
 #define FGL_SIGNAL_HPP
 
